a76c9054d73c12f809aa455e45b46bd0b9f6def3
Ashton Kemerling <akemerling@pivotallabs.com>
2013-11-27 12:03:45 -0700

    Who loves configuration? I do!

--- src/main/java/org/jenkinsci/plugins/Pivotal/TrackerDeliver.java
+++ src/main/java/org/jenkinsci/plugins/Pivotal/TrackerDeliver.java
@@ -1,10 +1,18 @@
 package org.jenkinsci.plugins.Pivotal;
 
 import hudson.Launcher;
+import hudson.Extension;
+
 import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
+import hudson.tasks.Publisher;
+import hudson.tasks.BuildStepDescriptor;
+
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.util.logging.Logger;
 
@@ -17,6 +25,7 @@ import java.util.logging.Logger;
  */
 public class TrackerDeliver extends Notifier {
     private final static Logger LOGGER = Logger.getLogger(TrackerDeliver.class.getName());
+    private String trackerToken;
 
     public BuildStepMonitor getRequiredMonitorService() {
         return BuildStepMonitor.BUILD;
@@ -27,4 +36,34 @@ public class TrackerDeliver extends Notifier {
         return true;
     }
 
+    @DataBoundConstructor
+    public TrackerDeliver(final String trackerToken) {
+        super();
+        this.trackerToken = trackerToken;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+        private String token;
+
+        public String getToken() {
+            return token;
+        }
+
+        @Override
+        public TrackerDeliver newInstance(StaplerRequest sr) {
+            if (token == null) token = sr.getParameter("trackerToken");
+            return new TrackerDeliver(token);
+        }
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Tracker Deliver";
+        }
+
+    }
 }
